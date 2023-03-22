3152b2fdbebec51702b3b77f9426f2e3882c72b4
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-03-22 10:27:18 -0300

    Refactor: Melhorias na conexão com o banco de dados

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-const  {Sequelize} = require('sequelize');
+const {Sequelize} = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_TYPE
 });
 
-sequelize.authenticate();
 try {
+    sequelize.authenticate();
     console.log('Conectado ao banco de dados com sucesso!');
 } catch (error) {
     console.erro('Erro: Erro ao conectar com o banco de dados: ', error);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const chamados = require('./routes/chamados');
 const app = express();
 const port = 3000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
