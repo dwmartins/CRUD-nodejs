5519d167eebd057b86acd32adef644a3caeffe93
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-03-23 09:06:12 -0300

    feat: Desenvolvida rota de tramaneto de erro de rotas não encontradas

--- index.js
+++ index.js
@@ -11,11 +11,26 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send(`<h2>Servidor em execução!</h2>`)
-})
+});
 
 app.use('/', chamados)
 app.use('/', tema);
+
+app.use((req, res, next) => {
+    const erro = new Error('Rota não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.json({
+        erro: {
+            erro: error.message
+        }
+    });
+});
  
 app.listen(port, () => {
     console.log(`Servidor em execução na porta: ${port}`);
-})
+});
